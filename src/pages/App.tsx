1c05a47971dd3a5d7f87bba472d9189ef0285e02
Brais Piñeiro <brapifra@gmail.com>
2018-02-26 16:57:53 +0100

    Basic userQuery

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { withApollo } from 'react-apollo';
+import { withApollo, Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import { Layout, Menu, Icon, Avatar } from 'antd';
 import Dashboard from './Dashboard/Dashboard';
@@ -14,78 +14,117 @@ interface State {
   page: string;
 }
 
+const userQuery = gql`
+  query Query ($email: String!, $coach: Boolean!) {
+    user (email: $email) {
+      name
+      photo
+    }
+    trainings (coach: $email) @include(if: $coach) {
+      registryDate
+    }
+  }
+`;
+
 class App extends React.Component<ApolloProps, State> {
   state: State = {
     page: 'dashboard'
   };
 
   public render() {
-    if (!localStorage.getItem('token')) {
-      return (
-        <Layout style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <Login
-            submit={async (args: any, onError: Function) => {
-              try {
-                const { data } = await this.props.client.query({
-                  query: gql`query Query($email: String!, $password: String!) {
+    console.log("App rendered");
+    const login = (
+      <Login
+        submit={async (args: any, onError: Function) => {
+          try {
+            const { data } = await this.props.client.query({
+              query: gql`query Query($email: String!, $password: String!) {
                     token(email: $email, password: $password){
                       error
                       token
                     }
                   }`,
-                  variables: args
-                });
-                if (data.token.error) {
-                  throw new Error(data.token.error);
-                }
-                localStorage.setItem('token', data.token.token);
-                this.forceUpdate();
-              } catch (e) {
-                onError(e.message);
-              }
-            }}
-          />
-        </Layout>
-      );
+              variables: args
+            });
+            if (data.token.error) {
+              throw new Error(data.token.error);
+            }
+            localStorage.setItem('token', data.token.token);
+            localStorage.setItem('email', args.email);
+            localStorage.setItem('type', 'COACH');
+            this.forceUpdate();
+          } catch (e) {
+            onError(e.message);
+          }
+        }}
+      />
+    );
+    if (!localStorage.getItem('token')) {
+      return login;
     }
     return (
-      <Layout style={{ height: '100vh' }}>
-        <Layout.Sider collapsed={true} style={{ boxShadow: '3px 0 5px -2px #888' }}>
-          <Menu
-            theme="dark"
-            mode="inline"
-            selectedKeys={[this.state.page]}
-            className="sider-menu"
-          >
-            <Menu.Item key="user">
-              <Avatar icon="user" />
-            </Menu.Item>
-            <Menu.Item key="dashboard">
-              <Icon type="dashboard" onClick={() => this.setState({ page: 'dashboard' })} />
-              <span>Dashboard</span>
-            </Menu.Item>
-            <Menu.Item key="trainings">
-              <Icon type="solution" onClick={() => this.setState({ page: 'trainings' })} />
-              <span>Trainings</span>
-            </Menu.Item>
-            <Menu.Item key="customers">
-              <Icon type="team" onClick={() => this.setState({ page: 'customers' })} />
-              <span>Customers</span>
-            </Menu.Item>
-            <Menu.Item key="calendar">
-              <Icon type="calendar" onClick={() => this.setState({ page: 'calendar' })} />
-              <span>Calendar</span>
-            </Menu.Item>
-            <Menu.Item key="billing">
-              <Icon type="credit-card" onClick={() => this.setState({ page: 'billing' })} />
-              <span>Billing</span>
-            </Menu.Item>
-          </Menu>
-        </Layout.Sider>
-        <Layout.Content style={{ margin: '16px' }}>
-          <Dashboard />
-        </Layout.Content>
-      </Layout>
+      <Query
+        query={userQuery}
+        variables={{
+          email: localStorage.getItem('email'),
+          coach: localStorage.getItem('type') === 'COACH'
+        } as any}
+      >
+        {({ loading, error, data, _ }) => {
+          if (loading) {
+            return (<span>Loading...</span>);
+          }
+          if (!error && data) {
+            console.log(data);
+            const user = (data as any).user;
+            const avatarProps = {
+              icon: 'user',
+              src: user.photo
+            };
+
+            return (
+              <Layout style={{ height: '100vh' }}>
+                <Layout.Sider collapsed={true} style={{ boxShadow: '3px 0 5px -2px #888' }}>
+                  <Menu
+                    theme="dark"
+                    mode="inline"
+                    selectedKeys={[this.state.page]}
+                    className="sider-menu"
+                  >
+                    <Menu.Item key="user">
+                      <Avatar {...avatarProps} />
+                    </Menu.Item>
+                    <Menu.Item key="dashboard">
+                      <Icon type="dashboard" onClick={() => this.setState({ page: 'dashboard' })} />
+                      <span>Dashboard</span>
+                    </Menu.Item>
+                    <Menu.Item key="trainings">
+                      <Icon type="solution" onClick={() => this.setState({ page: 'trainings' })} />
+                      <span>Trainings</span>
+                    </Menu.Item>
+                    <Menu.Item key="customers">
+                      <Icon type="team" onClick={() => this.setState({ page: 'customers' })} />
+                      <span>Customers</span>
+                    </Menu.Item>
+                    <Menu.Item key="calendar">
+                      <Icon type="calendar" onClick={() => this.setState({ page: 'calendar' })} />
+                      <span>Calendar</span>
+                    </Menu.Item>
+                    <Menu.Item key="billing">
+                      <Icon type="credit-card" onClick={() => this.setState({ page: 'billing' })} />
+                      <span>Billing</span>
+                    </Menu.Item>
+                  </Menu>
+                </Layout.Sider>
+                <Layout.Content style={{ margin: '16px' }}>
+                  <Dashboard />
+                </Layout.Content>
+              </Layout>
+            );
+          }
+          return login;
+        }}
+      </Query>
     );
   }
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, Checkbox, Layout } from 'antd';
 import './Login.css';
 const FormItem = Form.Item;
 
@@ -13,39 +13,41 @@ class Login extends React.Component<Props, {}> {
   public render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-          {getFieldDecorator('email', {
-            rules: [{ type: 'email', required: true, message: 'Email required!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Email" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Password required!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
+      <Layout style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <FormItem>
+            {getFieldDecorator('email', {
+              rules: [{ type: 'email', required: true, message: 'Email required!' }],
+            })(
+              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Email" />
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Password required!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Password"
+              />
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true,
+            })(
+              <Checkbox>Remember me</Checkbox>
+            )}
+            <a className="login-form-forgot" href="">Forgot password</a>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
           </Button>
-          Or <a href="">register now!</a>
-        </FormItem>
-      </Form>
+            Or <a href="">register now!</a>
+          </FormItem>
+        </Form>
+      </Layout>
     );
   }
 
