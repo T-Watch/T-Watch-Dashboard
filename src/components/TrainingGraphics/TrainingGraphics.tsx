06f40f084bbac97b3d19fb0b8168db0d26737a6e
Maria Losada <mariamolgas@gmail.com>
2018-04-26 18:19:38 +0200

    Speed unit m/s

--- src/components/TrainingGraphics/TrainingGraphics.tsx
+++ src/components/TrainingGraphics/TrainingGraphics.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Card } from '../../components';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { ResponsiveContainer } from 'recharts';
 
 interface TrainingGraphicsProps {
     training: object; 
@@ -16,7 +17,7 @@ class TrainingGraphics extends React.Component<any, TrainingGraphicsProps> {
     let HRspeed = [{HR: 0, speed: 0}];
     for (const tb of this.props.training.trainingBlocks) {
     for (const tbr of tb.result) {
-      let date = {HR: tbr.HR, speed: tbr.speed };
+      let date = {HR: tbr.HR, speed: tbr.speed, distance: tbr.distance };
       HRspeed.push(date);
     }
     }
@@ -25,11 +26,20 @@ class TrainingGraphics extends React.Component<any, TrainingGraphicsProps> {
     <Card title="HR - Speed">
         <ScatterChart width={w} height={300} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
             <XAxis dataKey={'HR'} type="number" name="HR" unit="bpm"/>
-            <YAxis dataKey={'speed'} type="number" name="speed" unit="km/h"/>
+            <YAxis dataKey={'speed'} type="number" name="speed" unit="m/s"/>
             <CartesianGrid />
             <Scatter name="HR - speed" data={HRspeed} fill="#8884d8"/>
             <Tooltip cursor={{strokeDasharray: '3 3'}}/>
         </ScatterChart>
+    </Card>
+    <Card title="Distance">
+    <ResponsiveContainer width="100%" height={100}>
+        <LineChart data={HRspeed}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey={'distance'} name="Distance" unit="m"/>
+              <Line type="monotone" dataKey={'HR'} name="HR" unit="bpm" stroke="#4286f4" strokeWidth={2} />
+        </LineChart>
+    </ResponsiveContainer>
     </Card>
    {/*} <Card title="HR2 - speed">
         <ScatterChart width={300} height={300} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
